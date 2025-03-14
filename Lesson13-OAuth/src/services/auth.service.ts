86cf543d9daa70fc57404df8d8f1201411cd4062
Dung Dev <103142140+DungPHPandREACT@users.noreply.github.com>
2025-03-14 21:53:31 +0700

    Update auth.service.ts

--- Lesson13-OAuth/src/services/auth.service.ts
+++ Lesson13-OAuth/src/services/auth.service.ts
@@ -1,46 +1,48 @@
-import passport from "passport";
+import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { googleConfig } from "../configs/googleConfig";
-import User from "../models/user.model";
-import { hashPassword } from "../utils/hashPassword";
+import { googleConfig } from '../configs/googleConfig';
+import User from '../models/user.model';
+import { hashPassword } from '../utils/hashPassword';
 
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: googleConfig.clientId,
-			clientSecret: googleConfig.clientSecret,
-			callbackURL: googleConfig.callbackURL,
-		},
-		async (
-			accessToken: string,
-			refreshToken: string,
-			profile: any,
-			done: Function
-		) => {
-			try {
-				const { id, emails, name } = profile;
-				const existingUser = await User.findOne({ googleId: id });
+export const initialOAuthService = () => {
+	passport.use(
+		new GoogleStrategy(
+			{
+				clientID: googleConfig.clientId,
+				clientSecret: googleConfig.clientSecret,
+				callbackURL: googleConfig.callbackURL,
+			},
+			async (
+				accessToken: string,
+				refreshToken: string,
+				profile: any,
+				done: Function
+			) => {
+				try {
+					const { id, emails, name } = profile;
+					const existingUser = await User.findOne({ googleId: id });
 
-				if (existingUser) {
-					return done(null, existingUser);
+					if (existingUser) {
+						return done(null, existingUser);
+					}
+
+					const password = 'randomPassword123';
+					const hashedPassword = await hashPassword(password);
+
+					const newUser = new User({
+						googleId: id,
+						email: emails[0].value,
+						firstName: name.givenName,
+						lastName: name.familyName,
+						password: hashedPassword,
+					});
+
+					await newUser.save();
+					return done(null, newUser);
+				} catch (error) {
+					return done(error, null);
 				}
-
-				const password = 'randomPassword123';
-				const hashedPassword = await hashPassword(password);
-
-				const newUser = new User({
-					googleId: id,
-					email: emails[0].value,
-					firstName: name.givenName,
-					lastName: name.familyName,
-					password: hashedPassword,
-				});
-
-				await newUser.save();
-				return done(null, newUser);
-			} catch (error) {
-				return done(error, null);
 			}
-		}
-	)
-);
+		)
+	);
+};
